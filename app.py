472f4193921f3ef906d9f053a808e244031051b8
James M. Farrelly <jimfarrelly27@gmail.com>
2024-04-08 18:52:14 -0500

    Updating

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # This route serves the homepage and returns index.html
     return render_template('index.html')
 
 @app.route('/about')
 def about():
-    # This route serves the about page and returns about.html
     return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
