028b16d0825ba1fd4325777f18c6b04e9aa8ff0a
James M. Farrelly <jimfarrelly27@gmail.com>
2024-04-08 18:23:16 -0500

    working

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # This route serves the homepage and returns index.html
     return render_template('index.html')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/about')
+def about():
+    # This route serves the about page and returns about.html
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
